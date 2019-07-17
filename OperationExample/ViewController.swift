dd32934a6d42358942c78e7dae5cffd3d8d732e9
Ryan <grossry@oregonstate.edu>
2019-07-16 20:13:58 -0400

    Readded initial project files

---
+++ OperationExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OperationExample
+//
+//  Created by Ryan Gross on 7/16/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
